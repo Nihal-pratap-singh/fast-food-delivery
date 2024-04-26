1fbdb82bb31d27d691fe360fe90a22ff92be15a5
Nihal singh <148356246+Nihal-pratap-singh@users.noreply.github.com>
2024-04-26 03:14:57 -0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://fastfood:fastfood7428@cluster0.8u0wxeu.mongodb.net/fastfood', {
+        await mongoose.connect('mongodb+srv://', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
